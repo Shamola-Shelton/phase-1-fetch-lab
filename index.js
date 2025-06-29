e69e885ba3ec09c5d89d51861de4a64cce41c2b7
Shamola Shelton <shamolashelton@gmail.com>
2025-06-29 17:32:03 +0300

    Makes changes to Index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch the books and return the fetch to allow test access
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -12,6 +14,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
